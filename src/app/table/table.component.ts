f6024a80e20a4082a5a3531609cc8b4e2320b9d6
Trung Vo <trungk18@gmail.com>
2022-09-03 15:20:14 +0800

    Add table

---
+++ src/app/table/table.component.ts
@@ -0,0 +1,110 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  createColumnHelper,
+  createTable,
+  getCoreRowModel,
+} from '@tanstack/table-core';
+
+type Person = {
+  firstName: string;
+  lastName: string;
+  age: number;
+  visits: number;
+  status: string;
+  progress: number;
+};
+
+const data: Person[] = [
+  {
+    firstName: 'tanner',
+    lastName: 'linsley',
+    age: 24,
+    visits: 100,
+    status: 'In Relationship',
+    progress: 50,
+  },
+  {
+    firstName: 'tandy',
+    lastName: 'miller',
+    age: 40,
+    visits: 40,
+    status: 'Single',
+    progress: 80,
+  },
+  {
+    firstName: 'joe',
+    lastName: 'dirte',
+    age: 45,
+    visits: 20,
+    status: 'Complicated',
+    progress: 10,
+  },
+];
+
+const columnHelper = createColumnHelper<Person>();
+
+const columns = [
+  columnHelper.accessor('firstName', {
+    cell: (info) => info.getValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor((row) => row.lastName, {
+    id: 'lastName',
+    cell: (info) => info.getValue(),
+    header: () => 'Last Name',
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('age', {
+    header: () => 'Age',
+    cell: (info) => info.renderValue(),
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('visits', {
+    header: () => 'Visits',
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('status', {
+    header: 'Status',
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('progress', {
+    header: 'Profile Progress',
+    footer: (info) => info.column.id,
+  }),
+];
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TableComponent implements OnInit {
+table = createTable({
+  data,
+  columns,
+  state: {},
+  onStateChange: () => {},
+  getCoreRowModel: getCoreRowModel(),
+  renderFallbackValue: null,
+});
+  
+
+  constructor() {}
+
+  ngOnInit(): void {
+    // this.table.setState(prev => ({
+    //   ...prev,
+    //   ...this.table.initialState
+    // }))
+    this.table.setOptions(prev => ({
+      ...prev,
+      state: {
+        ...prev.state,
+        ...this.table.initialState
+      }
+    }))
+    console.log('initial table state', this.table.initialState);
+    console.log('table state', this.table.getState());
+  }
+}
